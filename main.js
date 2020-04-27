e57c98f9435c4d113a964ec374d16cc24793330c
princejohnson37 <35042040+princejohnson37@users.noreply.github.com>
2020-04-27 20:08:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 var Java = ["fun()","hello"]; //function list 
 var Python = ["function()","hi"]; //coresponing function list
+var word = document.getElementById("input-text").value;
+var arrInp = document.getElementById("lang-select").value;
+var arrOut = document.getElementById("output-dropdown").value;
+var arrOutDisplay;
+if(arrInp=="Java"){
+	arr=Java;
+}
+else{
+	arr = Python;
+}
+if(arrOut=="Java"){
+	arrOutDisplay=Java;
+}
+else{
+	arrOutDisplay = Python;
+}
 function strcmp()
 {
     var i;
     var index=-1;
-    var word ; //input from text field
-    var arr ;  //value from input drop down options 
+   	//input from text field
+    //value from input drop down options 
 
     for(i=0;i<java_fun.length;i++)  
     {
@@ -16,12 +32,14 @@ function strcmp()
 
         } 
         //document.alert("ello");
+
     }
 
     if(index!=-1){      //output   
-    	document.alet(Python[index]);  
+    	document.alet(arrOutDisplay[index]);  
 		var para = document.querySelector('#output-html'); //output
 		para.innerHTML='<h2>this works</h2>';
-}	
+	}	
 
 
+}
